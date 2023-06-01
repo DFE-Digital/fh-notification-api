45a969ff75eb30b5b4fee6517340b4a82eababda
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-06-01 14:13:22 +0100

    Initial Create

---
+++ src/FamilyHubs.Notification.Data/Repository/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using FamilyHubs.Notification.Data.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FamilyHubs.Notification.Data.Repository;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+    public ApplicationDbContext
+        (
+            DbContextOptions<ApplicationDbContext> options,
+            AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor
+        )
+        : base(options)
+    {
+        _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+        optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+    }
+}
