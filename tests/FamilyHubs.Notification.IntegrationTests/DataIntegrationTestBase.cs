209e13c16f0a7cbd54f977b393df85f17be0d308
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-10-23 15:40:34 +0100

    Encrypt Notifications Database

--- tests/FamilyHubs.Notification.IntegrationTests/DataIntegrationTestBase.cs
+++ tests/FamilyHubs.Notification.IntegrationTests/DataIntegrationTestBase.cs
@@ -3,8 +3,10 @@ using AutoMapper.EquivalencyExpression;
 using FamilyHubs.Notification.Core;
 using FamilyHubs.Notification.Data.Interceptors;
 using FamilyHubs.Notification.Data.Repository;
+using FamilyHubs.SharedKernel.Security;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
@@ -35,6 +37,18 @@ public class DataIntegrationTestBase
         var mockIHttpContextAccessor = Mock.Of<IHttpContextAccessor>();
         var auditableEntitySaveChangesInterceptor = new AuditableEntitySaveChangesInterceptor(mockIHttpContextAccessor);
 
+        var inMemorySettings = new Dictionary<string, string?> {
+            {"Crypto:UseKeyVault", "False"},
+            {"Crypto:DbEncryptionKey", "188,7,221,249,250,101,147,86,47,246,21,252,145,56,161,150,195,184,64,43,55,0,196,200,98,220,95,186,225,8,224,75"},
+            {"Crypto:DbEncryptionIVKey", "34,26,215,81,137,34,109,107,236,206,253,62,115,38,65,112"},
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
+        IKeyProvider keyProvider = new KeyProvider(configuration);
+
         return new ServiceCollection().AddEntityFrameworkSqlite()
             .AddDbContext<ApplicationDbContext>(dbContextOptionsBuilder =>
             {
@@ -43,6 +57,7 @@ public class DataIntegrationTestBase
                     opt.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.ToString());
                 });
             })
+            .AddSingleton(keyProvider)
             .AddSingleton(auditableEntitySaveChangesInterceptor)
             .AddAutoMapper((serviceProvider, cfg) =>
             {
